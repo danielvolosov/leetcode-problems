059ba68edb27e49a6ac74501626f0b9cc234dd7e
daniel <danielvolosov@outlook.com>
2021-10-04 22:35:18 -0500

    Group anagrams

---
+++ src/group_anagrams/GroupAnagrams.java
@@ -0,0 +1,30 @@
+package group_anagrams;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List> result = new HashMap<>();
+
+        if (strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+
+            Arrays.sort(chars);
+
+            String key = String.valueOf(chars);
+
+            if (!result.containsKey(key)) {
+                result.put(key, new ArrayList());
+            }
+
+            result.get(key).add(str);
+        }
+
+        return new ArrayList(result.values());
+    }
+}
