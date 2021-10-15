496eedd18988303ba0abb886515f28e2de3a6c4e
daniel <danielvolosov@outlook.com>
2021-10-15 09:54:34 -0500

    intersection of two arrays 2

---
+++ src/intersection_of_two_arrays_ii/Solution.java
@@ -0,0 +1,29 @@
+package intersection_of_two_arrays_ii;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        int pointer = 0;
+        
+        for (int num : nums2) {
+            int count = map.getOrDefault(num, 0);
+            
+            if (count > 0) {
+                nums1[pointer++] = num;
+                map.put(num, count - 1);
+            }
+        }
+        
+        return Arrays.copyOfRange(nums1, 0, pointer);
+    }
+}
