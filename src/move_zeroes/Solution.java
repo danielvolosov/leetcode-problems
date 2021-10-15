6ec37ad8d990a0a1e4f4c486cd27663680472f94
daniel <danielvolosov@outlook.com>
2021-10-15 11:51:55 -0500

    move zeroes

---
+++ src/move_zeroes/Solution.java
@@ -0,0 +1,16 @@
+package move_zeroes;
+
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        for (int i = 0, pointer = 0; i < nums.length; i++) {
+            
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[pointer];
+                nums[pointer] = temp;
+                pointer++;
+            }
+        } 	
+    }
+}
